b13f9102678c033937a2ae5ee553b352568f8d90
Glahn Christian <glahnchristi@htwchur.ch>
2017-08-14 15:41:05 +0200

    hide toolbar if it is empty

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -150,8 +150,13 @@ class core_renderer extends \core_renderer {
         $context->header_menu = $this->context_header_settings_menu();
         $context->region_menu = $this->region_main_settings_menu_structure();
 
-        if (!(empty($context->header_menu) && empty($context->region_menu))) {
-            // !(empty($context->header_menu->secondary) && empty($context->regionmenu->secondary))) {
+        // if (!(empty($context->header_menu) && empty($context->region_menu))) {
+        if (!(empty($context->header_menu) ||
+              empty($context->header_menu->secondary->items)) ||
+            !(empty($context->region_menu) ||
+              empty($context->region_menu->secondaryactions))) {
+
+            // !(empty($context->header_menu->secondaryactions) && empty($context->regionmenu->secondaryactions))) {
             // hide the toolbar if nothing is visible.
             $context->show_toolbar = true;
         }
