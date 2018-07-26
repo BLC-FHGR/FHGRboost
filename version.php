ec2ec3dbfad0161866150389221c7a60fabba294
barrasroger <roger.barras@htwchur.ch>
2018-07-26 14:41:39 +0200

    fix #111 Login with AAI and dropdown for manual login section

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2018070900;
+$plugin->version = 2018070901;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2016112900.00;
